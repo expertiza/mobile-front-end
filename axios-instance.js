dc26af1ec5ff159242d8dccdce91ffe6fd23d673
KunmiaoYang <kyang12@ncsu.edu>
2018-11-06 13:47:11 -0500

    remove merge conflict in pakcage.json

--- axios-instance.js
+++ axios-instance.js
@@ -3,7 +3,7 @@ import { SecureStore } from 'expo';
 
 
 const instance = axios.create({
-    baseURL : 'http://192.168.1.12:3001/api/v1/'
+    baseURL : 'http://192.168.1.122:3000/api/v1/'
 });
 
 export default instance;
