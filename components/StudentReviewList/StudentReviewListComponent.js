01f92e4159abb24f282daac85733a76987f399d7
Ashwin <ashwinrisbood@gmail.com>
2018-12-13 11:00:24 -0500

    review list page

--- components/StudentReviewList/StudentReviewListComponent.js
+++ components/StudentReviewList/StudentReviewListComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {fetchAssignmentReviewData} from '../../redux/actions/StudentReview'
 // import {reviewNewTopic} from '../../redux/actions/StudentReview'
-import { ScrollView, Text, View, StyleSheet } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
-import Spinner from 'react-native-loading-spinner-overlay';
+import { ScrollView, Text, View, StyleSheet, Alert, Vibration, Switch } from 'react-native'
+import { CheckBox, ListItem, Button, Icon } from 'react-native-elements'
+
+import Spinner from 'react-native-loading-spinner-overlay'
 import moment from 'moment'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 
 
 class StudentReviewListComponent extends Component {
@@ -46,9 +47,78 @@ class StudentReviewListComponent extends Component {
         const part_id = navigation.getParam('par_id', 'NO-ID');
         if(this.props.non_reviewable_topics){
             return (
-                <View>
-                    <Text>LULZ Hello review options for id:{part_id}</Text>
-                </View>
+                <ScrollView style={styles.container}>
+                    <View style={{flex: 1}}>
+                        <Text style= {styles.titleText}>Reviews for "{this.props.assignment.name}"</Text>
+                        <Text h5>Number of reviews allowed:{this.props.assignment.num_reviews_allowed}</Text>
+                        <Text h5>You are required to do {this.props.assignment.num_reviews_required} reviews{"\n"}</Text>
+                        {(this.props.candidate_reviews_started.length>0)?
+                        //  <Table className="table_topic_list">
+                        <View>
+                            {this.props.candidate_reviews_started.map((i, index) => 
+                                <View key={"review"+index}>
+                                        <Text onPress = {()=>{
+                                            Vibration.vibrate(100);
+                                            
+                                                Alert.alert(
+                                                    'Developer Alert',
+                                                    'navigate to the review form',
+                                                    [
+                                                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                                                    {text: 'OK', onPress: () => console.log('OK Pressed')},
+                                                    ],
+                                                    { cancelable: false }
+                                                )}
+                                            } style={styles.linkText}>
+                                          {/* this.props.navigation.navigate('Screen name', {})}}style={styles.boldText}> */}
+                                             {i.id +":"+ i.name}
+                                        </Text>
+                                        {/* <NavLink to={`/response/view/${i.latest_response_id}`}> View </NavLink> */}
+                                        {/* add navlink to the edit form  */}
+                                        {/* <NavLink to={``}> edit </NavLink> */}
+                                        <Text style={{fontStyle:'italic'}}>
+                                            {"  -- last update  " +moment(new Date(i.map.updated_at)).format('MMMM Do YYYY, hh:mm')+"\n"}
+                                        </Text>
+                                </View>
+                            )}
+                        </View>
+                        : <View></View>
+                        }
+                    </View>
+                    <View>
+                        <CheckBox
+                            title="I don't care which topic I review"  
+                            checked={this.state.viewtopic} 
+                            onPress={this.toggletopic}
+                            containerStyle={{backgroundColor: "transparent" }}
+                            />
+                        <Text style={{fontWeight: "bold"}}> Select a topic below to begin a new review:</Text>
+                        {(this.state.viewtopic)?
+                            <View>
+                                { this.props.candidate_topics_to_review.map((e, index)=>
+                                        <View key={"topic_reviewable+"+index}>
+                                            {/* <Input type="radio" name="selected_topic" value={e.id} onChange={this.handleChange}/> */}
+                                                <Text>
+                                                    {e.topic_identifier + ":" + e.topic_name+"\n"}
+                                                </Text>
+                                        </View>
+                                )}
+                                {
+                                    this.props.non_reviewable_topics.map((e, index)=>
+                                    <View key={"topic_non_"+index}>
+                                        <Text style={{color:"gray"}}>
+                                            {e.topic_identifier + ":" + e.topic_name + "\n"} 
+                                        </Text>
+                                    </View>
+                                )}
+                            </View>:<View></View> 
+                        }
+                    </View>
+                    <Button onPress={()=> 
+                             this.newReview(this.props.assignment.id,this.props.profile_id,this.state.selected_topic)}
+                        title="Request a new submission to review">
+                    </Button>
+                </ScrollView>
             )
         }
         else{
@@ -59,11 +129,29 @@ class StudentReviewListComponent extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 20,
+      marginRight: 20,
+    },
+    titleText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+      },
+    linkText: {
+        fontWeight: 'bold',
+        color: "#986633"
+    }
+})
+
 const mapDispatchToProps = dispatch => {
     
     return {
         loadReviewData: (id,jwt) => dispatch(fetchAssignmentReviewData(id,jwt)),
-        //  reviewNewTopic: (assignment_id, user_id, topic_id) => dispatch(reviewNewTopic(assignment_id, user_id, topic_id))
+        reviewNewTopic: (assignment_id, user_id, topic_id) => dispatch(reviewNewTopic(assignment_id, user_id, topic_id))
     }
 }
 const mapStateToProps = state => {
